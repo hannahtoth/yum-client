de5938325ec297e2913e50a6f2928a783301a3f4
Jeremy Little <jlittle94@gmail.com>
2021-07-15 18:40:42 -0400

    fixed exposed bearer token

--- src/components/helper_functions/addRecipeToCookbook.js
+++ src/components/helper_functions/addRecipeToCookbook.js
@@ -17,7 +17,7 @@
                         }),
                         headers: new Headers({
                             'Content-Type': 'application/json',
-                            'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI2MTM3NTU0LCJleHAiOjE2MjYzMTAzNTR9.xP7yUawBbDWvq7YUzBpO34tKKWBW2axjd_u0erZxjCI`
+                            'Authorization': `Bearer ${sessionToken}`
                         })
                     })
                 let jsonData = await results.json()
