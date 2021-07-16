819ecea5c4865678d42dc24ea5861d7f9d1e9697
Jeremy Little <jlittle94@gmail.com>
2021-07-15 21:13:45 -0400

    Logout button and reformatting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-
-
-
-import {useState} from 'react';
-import './App.css';
-import AppLoggedIn from './components/AppLoggedIn';
-import Portal from './components/Portal';
-import Navbar from './components/Navbar';
-
+import "./App.css";
+import Portal from "./components/Portal";
 
 function App() {
-  
   return (
     <div className="App">
-
-     <Navbar />
-     {/* {loggedIn ? <AppLoggedIn /> : <Portal/>} */}
-     <Portal/>
-
+      <Portal />
     </div>
   );
 }
-
-
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,69 +1,78 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+  MenuIcon,
+  Avatar,
+  MenuItem,
+  Menu,
+} from "../materialuiexports";
 
-import { Button, Typography, AppBar, Toolbar, IconButton, MenuIcon, Avatar, MenuItem, Menu} from '../materialuiexports';
+const Navbar = (props) => {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+
+    title: {
+      flexGrow: 1,
+      padding: 2,
+      marginTop: 85,
+      textAlign: "left",
+    },
+
+    logo: {
+      maxWidth: 60,
+    },
+
+    button: {
+      marginTop: 60,
+    },
+  }));
 
 
-const Navbar = ({}) => {
 
+  const classes = useStyles();
 
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="static"
+        style={{
+          backgroundColor: "#789174",
+        }}
+      >
+        <Toolbar>
+          <img src="yumLogo.png" alt="logo" className={classes.logo} />
 
-const useStyles = makeStyles((theme) => ({
-        root: {
-          flexGrow: 1,
-        },
-        
-        title: {
-            flexGrow: 1,
-            padding: 2,
-            marginTop: 85,
-            textAlign: 'left'
-        
-         
-        },
+          <Typography
+            variant="h8"
+            style={{
+              color: "white",
+            }}
+            className={classes.title}
+          >
+            where recipes find you
+          </Typography>
+          {props.loggedIn ? 
+          <Button
+            color="inherit"
+            style={{
+              color: "white",
+            }}
+            className={classes.button}
+            onClick={props.clearToken}
+          >
+            Logout
+          </Button> : <></>}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
-        logo: {
-            maxWidth: 60,
-
-           
-          },
-
-          button: {
-              
-              marginTop: 60,
-          }
-
-      }));
-
-     
-      
-        const classes = useStyles();
-      
-        return (
-          <div className={classes.root}>
-            <AppBar position="static" style={{
-              backgroundColor: "#789174"
-            }}>
-              <Toolbar>
-                <img src='yumLogo.png' alt='logo' className={classes.logo} />
-                
-               
-                <Typography variant="h8"
-                
-                style={{
-                    color: 'white'
-                }} className={classes.title}>
-                where recipes find you
-                </Typography>
-            
-                <Button color="inherit" 
-                style={{
-                    color: 'white'
-                }} className={classes.button}>Login</Button>
-              </Toolbar>
-            </AppBar>
-          </div>
-        );
-}
-
-        export default Navbar;
+export default Navbar;
